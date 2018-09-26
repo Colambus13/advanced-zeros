b191bb661846f4493bf8368294f757d92d85c7ed
Pavel Shestak <pavelshestak29@gmail.com>
2018-09-27 00:25:04 +0300

    'eight_task'

--- src/index.js
+++ src/index.js
@@ -1,3 +1,61 @@
-module.exports = function getZerosCount(number, base) {
-  // your implementation
-}
+module.exports = function getZerosCount(number, base){
+
+    const solutionArr = countZeroes(number, base);
+    let zeroCounter = solutionArr[0];
+
+    solutionArr.forEach(function(item){
+        if (item < zeroCounter) {
+            zeroCounter = item;
+        }
+    });
+    return zeroCounter;
+};
+
+const gettingBasePowers = function getBasePrimePowers(numBase){
+    let i = 1,
+        j = 2;
+    let baseSystemNotationArray = [];
+
+    while (j < numBase) {
+        if (numBase % j === 0){
+            baseSystemNotationArray[i] = j;
+            numBase = numBase / j;
+            i++;
+        }else {
+            j++;
+        }
+    }
+    baseSystemNotationArray[i] = j;
+
+    const baseSystemNotation = {};
+    baseSystemNotationArray.forEach(function(item){
+        if (!baseSystemNotation[item]){
+            baseSystemNotation[item] = 1;
+        }else{
+            baseSystemNotation[item]++;
+        }
+    });
+
+    return baseSystemNotation;
+};
+
+const countZeroes = function countZeroesAllSystems(number, base){
+
+    let iteration = number;
+    let decisionArr = [];
+    const baseSystemNotation = gettingBasePowers(base);
+
+    for (let item in baseSystemNotation){
+        let itemSum = 0;
+        let itemAccumulator = +item;
+
+        while (itemAccumulator <= iteration){
+            itemSum += Math.floor(iteration/itemAccumulator);
+            itemAccumulator *= +item;
+        }
+
+        itemSum = Math.floor(itemSum/baseSystemNotation[item]);
+        decisionArr.push(itemSum);
+    }
+    return decisionArr;
+};
